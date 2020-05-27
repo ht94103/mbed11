18fcd8ab85d3a8adae69c9b0d581b1b7d9b2a0c4
ht94103 <ee106061271@gapp.nthu.edu.tw>
2020-05-27 18:12:13 +0800

    Demo

---
+++ 11_Demo/XBee_host.py
@@ -0,0 +1,89 @@
+import serial
+
+import time
+
+
+# XBee setting
+
+serdev = '/dev/ttyUSB0'
+
+s = serial.Serial(serdev, 9600)
+
+
+s.write("+++".encode())
+
+char = s.read(2)
+
+print("Enter AT mode.")
+
+print(char.decode())
+
+
+s.write("ATMY 0x187\r\n".encode())
+
+char = s.read(3)
+
+print("Set MY 0x187.")
+
+print(char.decode())
+
+
+s.write("ATDL 0x178\r\n".encode())
+
+char = s.read(3)
+
+print("Set DL 0x178.")
+
+print(char.decode())
+
+
+s.write("ATID 0x3\r\n".encode())
+
+char = s.read(3)
+
+print("Set PAN ID 0x3.")
+
+print(char.decode())
+
+
+s.write("ATWR\r\n".encode())
+
+char = s.read(3)
+
+print("Write config.")
+
+print(char.decode())
+
+
+s.write("ATMY\r\n".encode())
+
+char = s.read(4)
+
+print("MY :")
+
+print(char.decode())
+
+
+s.write("ATDL\r\n".encode())
+
+char = s.read(4)
+
+print("DL : ")
+
+print(char.decode())
+
+
+s.write("ATCN\r\n".encode())
+
+char = s.read(3)
+
+print("Exit AT mode.")
+
+print(char.decode())
+
+
+print("start sending RPC")
+
+s.write("/myled1/write 1\r".encode())
+
+s.close()
